b5cd8d4962c18930adc518ebee196370737ccd53
Adhiraj Nandy <Adhiraj.Nandy@Geminisolutions.com>
2024-06-13 10:35:48 +0530

    Update Font UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo.png'
 const Navbar = () => {
   return (
     <>
-        <nav className="bg-indigo-700 border-b border-indigo-500">
+      <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
           <div
@@ -17,7 +17,7 @@ const Navbar = () => {
                 src={logo}
                 alt="React Jobs"
               />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
+              <span className="hidden md:block text-white text-2xl font-semibold ml-2"
                 >React Jobs</span
               >
             </a>
